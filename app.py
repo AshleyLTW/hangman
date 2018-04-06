3de0ee573d71c3eb9889c03b67961e59a8aa4678
AshleyLTW <missashleywong@gmail.com>
2018-04-06 13:05:41 +1000

    level input now redirects to new page

--- app.py
+++ app.py
@@ -1,21 +1,37 @@
 from flask import Flask
 from flask import render_template
+from flask import request
+from flask import redirect, url_for
 import random 
 import wordpile
-import cgi
-import cgitb # To remove later
 
 app = Flask(__name__)
-cgitb.enable()
 
 @app.route('/', methods=['POST', 'GET'])
-def selector():
-		wordSplit = []
-		wordNumber = random.randint(0, len(wordpile.words)-1) 
-		wordSelected = f"{wordpile.words.pop(wordNumber)}"
-		for character in wordSelected:
-			wordSplit.append(character)
-		return render_template("index.html", wordSplit=wordSplit)	
+
+def index():
+	if request.method == "POST":
+		level = request.form['level']
+		if level == 'easy' or level == 'Easy':
+			return redirect(url_for('testing')) # url_for links to the function that is called, not the url
+			# return redirect('easy' is also valid, but then your website breaks if you change the url)
+		elif level == 'hard' or level == 'Hard':
+			return redirect(url_for('testing1'))
+		else:
+			error = "I don't understand, please try again"
+			return render_template("index.html", error=error)
+	else:
+		return render_template("index.html")	
+
+@app.route('/easy')
+def testing():
+	wordSplit = "ez"
+	return render_template("guessing.html", wordSplit=wordSplit)
+
+@app.route('/hard')
+def testing1():
+	wordSplit = "hz"
+	return render_template("guessing.html", wordSplit=wordSplit)
 
 
 
