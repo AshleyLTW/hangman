c18625938357d2b21199266c90231ca1001dd5c9
AshleyLTW <missashleywong@gmail.com>
2018-04-06 12:03:31 +1000

    added front-end of difficulty interface

--- app.py
+++ app.py
@@ -2,17 +2,20 @@ from flask import Flask
 from flask import render_template
 import random 
 import wordpile
+import cgi
+import cgitb # To remove later
 
 app = Flask(__name__)
+cgitb.enable()
 
 @app.route('/', methods=['POST', 'GET'])
 def selector():
-	wordSplit = []
-	wordNumber = random.randint(0, len(wordpile.words)-1) 
-	wordSelected = f"{wordpile.words.pop(wordNumber)}"
-	for character in wordSelected:
-		wordSplit.append(character)
-	return render_template("index.html", wordSplit=wordSplit)
+		wordSplit = []
+		wordNumber = random.randint(0, len(wordpile.words)-1) 
+		wordSelected = f"{wordpile.words.pop(wordNumber)}"
+		for character in wordSelected:
+			wordSplit.append(character)
+		return render_template("index.html", wordSplit=wordSplit)	
 
 
 
