e750fcc95dd8f6d79f571ba0bd97667b06aa6cb7
AshleyLTW <missashleywong@gmail.com>
2018-04-06 11:26:52 +1000

    words removed from list after chosen

--- app.py
+++ app.py
@@ -1,14 +1,17 @@
 from flask import Flask
 from flask import render_template
 import random 
-import wordlist
+import wordpile
 
 app = Flask(__name__)
 
 @app.route('/', methods=['POST', 'GET'])
 def selector():
-	wordNumber = random.randint(0, len(wordlist.words)-1) 
-	return wordlist.words[wordNumber]
+	wordNumber = random.randint(0, len(wordpile.words)-1) 
+	wordSelected = wordpile.words.pop(wordNumber)
+	return wordSelected
+
+
 
 if __name__ == "__main__":
 	app.run()
