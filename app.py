a4c0319ef8d5b1ad8d06c54dd2c50765d9ac1a5e
AshleyLTW <missashleywong@gmail.com>
2018-04-22 21:43:21 +1000

    sophisticated theme for index

--- app.py
+++ app.py
@@ -59,6 +59,9 @@ def hard():
 		level = "hard"
 		return render_template("guessing.html")
 
+@app.route('/test')
+def test():
+	return render_template("test.html")
 
 if __name__ == "__main__":
 	app.run()
